1cb49418b60255d4be7d37755af8a45a1ac44bbd
Bhimashankar <bhimamalbhage@hotmail.com>
2020-07-29 22:43:13 +0530

    Updates

---
+++ precache-manifest.15a8066cf700810e544713a2b722a90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b487afb1eb03aa23ffcb5b795ac99ca5",
+    "url": "/bhimamalbhage/todo-react-app.git/index.html"
+  },
+  {
+    "revision": "daa29c82cfb1209ac3a9",
+    "url": "/bhimamalbhage/todo-react-app.git/static/css/main.cceb00d9.chunk.css"
+  },
+  {
+    "revision": "7a7f2da19d8a302c5254",
+    "url": "/bhimamalbhage/todo-react-app.git/static/js/2.6e1b2e35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bhimamalbhage/todo-react-app.git/static/js/2.6e1b2e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa29c82cfb1209ac3a9",
+    "url": "/bhimamalbhage/todo-react-app.git/static/js/main.f005898f.chunk.js"
+  },
+  {
+    "revision": "4a539ceec1df9d1b0534",
+    "url": "/bhimamalbhage/todo-react-app.git/static/js/runtime-main.baed1ee6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bhimamalbhage/todo-react-app.git/precache-manifest.15a8066cf700810e544713a2b722a90e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bhimamalbhage/todo-react-app.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
